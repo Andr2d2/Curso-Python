b92e128770c130083e846a05ded5733b886497a0
André "Dedé" Santos <andre.o.santos@globalhitss.com.br>
2025-09-10 22:30:34 -0300

    feat: 008 converter metros em centímetros e milímetros

---
+++ 0008-conversorDeMedidas.py
@@ -0,0 +1,6 @@
+meter = float(input('Digite um valor em metros: '))
+centimeter = meter * 100
+milimeter = meter * 1000
+
+print('{0} metro são {1} centímetros'.format(meter, centimeter))
+print('{0} metro são {1} milímetros'.format(meter, milimeter))
