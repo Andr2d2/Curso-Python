a02095e03e1a4088a0019c2419a8bc2b58e53e67
André "Dedé" Santos <andre.o.santos@globalhitss.com.br>
2025-09-10 21:01:50 -0300

    feat: 005 antecessor e sucessor de um número inteiro

---
+++ .vscode/005-antecessorSucessor.py
@@ -0,0 +1,5 @@
+currentNumber = int(input('DIgite um número:'))
+predecessor = currentNumber - 1
+successor = currentNumber + 1
+
+print('O antecessor de {0} é {1} e o sucessor é {2}'.format(currentNumber, predecessor, successor))
