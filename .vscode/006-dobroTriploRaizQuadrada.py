30d8d09b711fb2856d5762ac6e26a867b3db4a34
André "Dedé" Santos <andre.o.santos@globalhitss.com.br>
2025-09-10 22:00:30 -0300

    feat: 006 o dobro,, o triplo e a raiz quadrada de um número

---
+++ .vscode/006-dobroTriploRaizQuadrada.py
@@ -0,0 +1,8 @@
+num = int(input('Digite um número '))
+double = num * 2
+triple = num * 3
+squareRoot = int(num ** (1/2))
+
+print('O dobro de {0} é {1}'.format(num, double))
+print('O triplo de {0} é {1}'.format(num, triple))
+print('A raiz quadrada de {0} é {1}'.format(num, squareRoot))
